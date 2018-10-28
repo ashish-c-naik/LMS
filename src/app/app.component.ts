a9310adaaae465748fdbcbb612b7f124d5393a25
Ashish Naik <ashishnaik121@gmail.com>
2018-10-28 17:37:51 -0400

    Added sidebar and toolbar to the main display page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'KASND';
+  opened: boolean;
+  @ViewChild('drawer') sideNav: MatSidenav;
 }
