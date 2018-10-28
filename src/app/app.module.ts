fab4ca7efbe3a2241e5301f27f2fae14eb62493a
rashikak295 <36055710+rashikak295@users.noreply.github.com>
2018-10-28 00:10:44 -0400

    Initial commits. Installed required modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+import { MatMenuModule, MatButtonModule, MatIconModule, MatToolbarModule, MatListModule } from '@angular/material';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
